995367b0c28c2d5080d0a5d187f32db731c770c7
ATartarus <kolyapryazko@gmail.com>
2024-05-31 15:34:32 +0300

    remove build

--- out/models/token.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Token {
-    constructor(user_id, token) {
-        this.user_id = user_id;
-        this.token = token;
-    }
-}
-exports.default = Token;
